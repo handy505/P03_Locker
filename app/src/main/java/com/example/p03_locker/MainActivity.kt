c8a43573824e03399f3734433f4d49c61b123087
handy <handy8831505@gmail.com>
2020-11-11 15:20:42 +0800

    加入一些橙要的圖片

--- app/src/main/java/com/example/p03_locker/MainActivity.kt
+++ app/src/main/java/com/example/p03_locker/MainActivity.kt
@@ -19,10 +19,16 @@ class MainActivity : AppCompatActivity() {
         locker.set_password(10)
         locker.lock()
 
+        val imageList = listOf(R.drawable.achi,
+                R.drawable.hwehwe,
+                R.drawable.luma,
+                R.drawable.tenten,
+                R.drawable.xiaoli,
+                R.drawable.yakult)
 
         val addbutton: Button = findViewById(R.id.addbutton)
         addbutton.setOnClickListener {
-            var s: String = guessnumTextView.text as String
+            val s: String = guessnumTextView.text as String
             var n: Int = s.toInt()
             if (n < 20){
                 n += 1
@@ -32,7 +38,7 @@ class MainActivity : AppCompatActivity() {
 
         val decbutton: Button = findViewById(R.id.decbutton)
         decbutton.setOnClickListener {
-            var s: String = guessnumTextView.text as String
+            val s: String = guessnumTextView.text as String
             var n: Int = s.toInt()
             if (n > 0){
                 n -= 1
@@ -40,19 +46,26 @@ class MainActivity : AppCompatActivity() {
             guessnumTextView.setText(n.toString())
         }
 
+
         val guessbutton: Button = findViewById(R.id.guessbutton)
         guessbutton.setOnClickListener {
-            var s: String = guessnumTextView.text as String
-            var n: Int = s.toInt()
+            val s: String = guessnumTextView.text as String
+            val n: Int = s.toInt()
 
             val ret: Boolean = locker.unlock(n)
-            val toast = Toast.makeText(this, ret.toString(), Toast.LENGTH_SHORT)
-            toast.show()
             if (ret) {
-                imageview.setImageResource(R.drawable._1)
+                val img = imageList[Random.nextInt(0, imageList.size)]
+                imageview.setImageResource(img)
             } else {
                 imageview.setImageResource(R.drawable.lock)
-
+                val hint = locker.get_hint_whether_is_it_bigger()
+                if (hint) {
+                    val toast = Toast.makeText(this, "Too Big", Toast.LENGTH_SHORT)
+                    toast.show()
+                } else {
+                    val toast = Toast.makeText(this, "Too Small", Toast.LENGTH_SHORT)
+                    toast.show()
+                }
             }
         }
 
@@ -60,7 +73,7 @@ class MainActivity : AppCompatActivity() {
         resetbutton.setOnClickListener {
             imageview.setImageResource(R.drawable.lock)
 
-            var n: Int = Random.nextInt(0, 20)
+            val n: Int = Random.nextInt(0, 20)
             locker.set_password(n)
             locker.lock()
         }
--- app/src/test/java/com/example/p03_locker/ExampleUnitTest.kt
+++ app/src/test/java/com/example/p03_locker/ExampleUnitTest.kt
@@ -10,67 +10,63 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
 
     @Test
     fun test_Locker_init_is_unlocked() {
-        var locker = Locker()
+        val locker = Locker()
         assertEquals(false, locker.locked)
     }
 
     @Test
     fun test_Locker_try_to_set_password_when_opened() {
-        var locker = Locker()
-        var ret: Boolean = locker.set_password(5)
+        val locker = Locker()
+        val ret: Boolean = locker.set_password(5)
         assertEquals(true, ret)
     }
 
     @Test
     fun test_Locker_try_to_set_password_when_locked() {
-        var locker = Locker()
+        val locker = Locker()
         locker.lock()
-        var ret: Boolean = locker.set_password(5)
+        val ret: Boolean = locker.set_password(5)
         assertEquals(false, ret)
     }
 
 
     @Test
     fun test_unlock_default_password(){
-        var locker = Locker()
+        val locker = Locker()
         locker.lock()
-        var ret: Boolean = locker.unlock(0)
+        val ret: Boolean = locker.unlock(0)
         assertEquals(true, ret)
     }
 
 
     @Test
     fun test_unlock_with_correct_password(){
-        var locker = Locker()
+        val locker = Locker()
         locker.set_password(5)
         locker.lock()
-        var ret: Boolean = locker.unlock(5)
+        val ret: Boolean = locker.unlock(5)
         assertEquals(true, ret)
     }
 
     @Test
     fun test_unlock_with_error_password(){
-        var locker = Locker()
+        val locker = Locker()
         locker.set_password(5)
         locker.lock()
-        var ret: Boolean = locker.unlock(4)
+        val ret: Boolean = locker.unlock(4)
         assertEquals(false, ret)
     }
 
     @Test
     fun test_get_diff(){
-        var locker = Locker()
+        val locker = Locker()
         locker.set_password(5)
         locker.lock()
         locker.unlock(4)
-        var hint: Int = locker.get_diff()
+        val hint: Int = locker.get_diff()
         assertEquals(1, hint)
 
         locker.unlock(7)
@@ -79,14 +75,14 @@ class ExampleUnitTest {
 
     @Test
     fun test_get_hint_whether_is_it_bigger() {
-        var locker = Locker()
+        val locker = Locker()
         locker.set_password(5)
         locker.lock()
         locker.unlock(4)
         assertEquals(false, locker.get_hint_whether_is_it_bigger())
 
-        //locker.unlock(6)
-        //assertEquals(true, locker.get_hint_whether_is_it_bigger())
+        locker.unlock(6)
+        assertEquals(true, locker.get_hint_whether_is_it_bigger())
 
     }
 
